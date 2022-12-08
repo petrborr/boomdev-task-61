4629aee1e0394d76e7e8f838ec853b22f2429d2f
petrborr <petrborr@gmail.com>
2022-12-08 13:38:23 +0200

    add: fire emoji to prices with class 'hot'

--- src/js/app.js
+++ src/js/app.js
@@ -2,4 +2,10 @@ import "../scss/app.scss";
 
 window.addEventListener("DOMContentLoaded", () => {
   // This block will be executed once the page is loaded and ready
+  const products = document.getElementsByClassName("price")
+  for (let product of products) {
+    if (product.classList.contains("hot")) {
+      product.innerHTML += " 🔥"
+    }
+  }
 });
